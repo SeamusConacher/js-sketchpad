ba2730dfe3547163587d4b645cbcf7ba1a52088a
Seamus Conacher <seamusconacher@gmail.com>
2016-07-07 22:09:59 +0100

    added default reset values and handling of incorrect inputs

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 	var dimensionY = 64;
 	var dimensionX = 64;
-  	var totalDim = 600;
-  	var bord = 0;
-  	var increment = 0.05;
+  var totalDim = 600;
+  var bord = 0;
+  var increment = 0.05;
   
-  	var canvasGen = function(){
-  		$('.container').empty();
+  var canvasGen = function(){
+  	$('.container').empty();
 
 		for(var i = 0; i < dimensionY; i++){
   			var row = ('row' + i);
@@ -17,36 +17,57 @@ $(document).ready(function(){
 		}
 
 		var boxDimX = Math.floor((totalDim - (2 * dimensionX * bord)) / dimensionX);
-  		var boxDimY = Math.floor((totalDim - (2 * dimensionY * bord)) / dimensionY);
+  	var boxDimY = Math.floor((totalDim - (2 * dimensionY * bord)) / dimensionY);
   			
-  		$('.container').css("width", totalDim + "px");
-  		$('.container').css("height", totalDim + "px");
+  	$('.container').css("width", totalDim + "px");
+  	$('.container').css("height", totalDim + "px");
  
  		$('.row').css("height", (boxDimY + (2 * bord)) + "px");
-  		$('.row').css("width", totalDim + "px");
+  	$('.row').css("width", totalDim + "px");
   
-  		$('.box').css("width", boxDimX + "px");
-  		$('.box').css("height", boxDimY + "px");
-  		$('.box').css("border", bord + "px solid white");
+  	$('.box').css("width", boxDimX + "px");
+  	$('.box').css("height", boxDimY + "px");
+  	$('.box').css("border", bord + "px solid white");
 
 
-    	$(".box").hover(function(){
-  			var opa = parseFloat(($(this).css("opacity")));
-    		opa += increment;
-    		if(opa > 1) {
-    		opa = 1;
-    		}
-  			$(this).css("opacity", opa);
-  		});
-  	}
+   	$(".box").hover(function(){
+      var opa = parseFloat(($(this).css("opacity")));
+   	  opa += increment;
+   	  if(opa > 1) {
+   	    opa = 1;
+   		}
+  		$(this).css("opacity", opa);
+  	});
+  }
 
-  	canvasGen();
+  canvasGen();
 
-  	$('button').click(function() {
-  		dimensionX = parseInt($('input[name=XDim]').val());
-  		console.log(dimensionX);
-    	dimensionY = parseInt($('input[name=YDim]').val());
-    	totalDim = parseInt($('input[name=TotDim]').val());
-    	canvasGen();
-    });
+  $('button').click(function() {
+    newX = parseInt($('input[name=XDim]').val());
+
+    if(isNaN(newX)){
+      dimensionX = 64;
+    }
+    else{
+      dimensionX = newX;
+    }
+
+    newY = parseInt($('input[name=XDim]').val());
+    if(isNaN(newY)){
+      dimensionY = 64;
+    }
+    else{
+      dimensionY = newY;
+    }
+
+    newDim = parseInt($('input[name=TotDim]').val());
+    if(isNaN(newDim)){
+      totalDim = 600;
+    }
+    else{
+      totalDim = newDim;
+    }
+    
+    canvasGen();
+  });
 });
